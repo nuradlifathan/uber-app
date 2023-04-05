4ccfaf89637031c70ca89f9fe315900e34248eba
ADLI <nuradlifathan@gmail.com>
2023-04-05 13:07:57 +0700

    Initial Commit

---
+++ uber-app/uber_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  uber_appApp.swift
+//  uber-app
+//
+//  Created by Adli on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct uber_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
